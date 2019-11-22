174f4fa5f59b319d8549d185c78c91b69ad0ec26
Alex Ambrioso <aambrioso1@gmail.com>
2019-11-22 09:56:47 -0500

    Add files via upload

--- mouse.py
+++ mouse.py
@@ -1,19 +1,22 @@
 import pygame, sys
-import random
 from pygame.locals import *
 
 """
 Prints out mouse event numbers when cursor is in the display window:
     
 1 = left click
-2 = right and left click simultaneously
+2 = right and left click simultaneously or scroll push
 3 = right click
+4 = scroll up
+5 = scroll down
 """
 
 pygame.init()
 pygame.display.set_mode((300,200))
 pygame.display.set_caption('Mouse Input Demonstration')
+
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -24,5 +27,7 @@ while running:
             print('Button down event:', event.button)
         if event.type == MOUSEBUTTONUP:
             print('Button up event:', event.button)
+        if event.type == MOUSEMOTION:
+            print('Mouse motion event:', pygame.mouse.get_pos())
 
 pygame.display.quit()
